bd03982aed10feccd15757cb4d33502c8776d9c0
mohadese zarrin <m.zarrin95@gmail.com>
2022-10-25 20:29:36 +0330

    REFACTOR:add node  to props and compelet action function

--- src/Components/Tree/node.tsx
+++ src/Components/Tree/node.tsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import { NodeType } from '../../types';
-import {ContextMenuTriggerEx, ContextMenuItemEx, ContextMenuEx } from '../ContextMenu';
+import { ContextMenuTriggerEx, ContextMenuItemEx, ContextMenuEx } from '../ContextMenu';
 
 interface Props {
-	node: NodeType;
-	handleContextMenuClick: (key: string) => void;
+  node: NodeType;
+  handleContextMenuClick: (action: string, node: NodeType) => void;
 }
 
-function Node({node, handleContextMenuClick}: Props) {
-	return (
+function Node({ node, handleContextMenuClick }: Props) {
+  return (
     <div>
       {/* NOTICE: id must be unique between EVERY <ContextMenuTrigger> and <ContextMenu> pair */}
       {/* NOTICE: inside the pair, <ContextMenuTrigger> and <ContextMenu> must have the same id */}
-			<ContextMenuTriggerEx
+      <ContextMenuTriggerEx
         id={node.key}
         title={node.title}
-       />
-         
-      <ContextMenuEx  id={node.key}>
-        <ContextMenuItemEx handleClick={handleContextMenuClick('ACTION1')} title={'افزودن زیرشاخه'}/>
-        <ContextMenuItemEx handleClick={handleContextMenuClick('ACTION2')} title={'برش'}/>
-        <ContextMenuItemEx handleClick={handleContextMenuClick('ACTION3')} title={'چسباندن'}/>
-        <ContextMenuItemEx handleClick={handleContextMenuClick('ACTION4')} title={'حذف'}/>
+      />
+
+      <ContextMenuEx id={node.key}>
+        <ContextMenuItemEx handleClick={() => handleContextMenuClick('ADD_BRANCH', node)} title={'افزودن زیرشاخه'} />
+        <ContextMenuItemEx handleClick={() => handleContextMenuClick('CUT', node)} title={'برش'} />
+        <ContextMenuItemEx handleClick={() => handleContextMenuClick('PASTE', node)} title={'چسباندن'} />
+        <ContextMenuItemEx handleClick={() => handleContextMenuClick('DELETE', node)} title={'حذف'} />
       </ContextMenuEx>
- 
+
     </div>
   );
 }
