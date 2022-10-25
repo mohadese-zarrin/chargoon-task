2c2d73da6e3648f638fa9ff0d025ba454897fcb0
mohadese zarrin <m.zarrin95@gmail.com>
2022-10-25 18:11:20 +0330

    FEAT:implement users table component

--- src/Components/Table/index.tsx
+++ src/Components/Table/index.tsx
@@ -1,6 +1,34 @@
 import React from 'react';
-interface Props { }
-function Table({ }: Props) {
-	return;
+import { UserType } from '../../types'
+import { Checkbox } from 'antd';
+import type { CheckboxChangeEvent } from 'antd/es/checkbox';
+
+interface Props {
+	userList: UserType[]
+	deleteUser: (user: UserType) => void,
+	setDefaultUser: (user: UserType) => void
+}
+
+
+function Table({ userList, deleteUser, setDefaultUser }: Props) {
+	const onCheck = (e: CheckboxChangeEvent, user: UserType) => {
+		setDefaultUser(user)
+	};
+	return (
+		<table className='users-table'>
+			<tr>
+				<th>عملیات</th>
+				<th>پیشفرض</th>
+				<th>نام</th>
+			</tr>
+			{userList.map((user) =>
+				<tr>
+					<td onClick={() => deleteUser(user)}>حذف</td>
+					<td><Checkbox defaultChecked={user.isDefault} onChange={(e) => onCheck(e, user)} /></td>
+					<td>{user.title}</td>
+				</tr>
+			)}
+		</table>
+	)
 }
 export default Table
